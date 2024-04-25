36e7d57938403d45f560fca63ecfe43c767b3bc9
Melvin Petracca <38191338+melvin2504@users.noreply.github.com>
2024-04-25 16:36:41 +0200

    Update main.py

--- FlaskApp/main.py
+++ FlaskApp/main.py
@@ -15,6 +15,27 @@ YOUR_HASH_PASSWD = "8eac4757d3804403cb4bbd4015df9d2ad252a1e6890605bacb19e5a01a5f
 
 app = Flask(__name__)
 
+OPENWEATHER_API_KEY = "6b85e31b08c576ddd0a8f6a60e5afc01"
+OPENWEATHER_URL = "https://api.openweathermap.org/data/3.0/weather"
+
+
+def get_outdoor_weather(city):
+    """Fetch outdoor temperature and humidity data from OpenWeather API for a given city."""
+    params = {
+        'q': city,
+        'appid': OPENWEATHER_API_KEY,
+        'units': 'metric'  # Use 'imperial' for Fahrenheit
+    }
+    response = requests.get(OPENWEATHER_URL, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        return {
+            "outdoor_temp": data['main']['temp'],
+            "outdoor_humidity": data['main']['humidity']
+        }
+    else:
+        raise Exception("Failed to fetch weather data")
+
 # get the column names of the db
 # PROJECT_ID.DATASET_ID.weather-records
 q = """
@@ -29,12 +50,12 @@ def send_to_bigquery():
         if request.get_json(force=True)["passwd"] != YOUR_HASH_PASSWD:
             raise Exception("Incorrect Password!")
         data = request.get_json(force=True)["values"]
-        # For exercise 2: Call the openweatherapi and add the resulting 
-        # values to the `data` dictionary
-        # data["outdoor_temp"] = ...
-        # data["outdoor_humidity"] = ...
-        # data["weather"] = ...
-        # building the query
+
+        
+        weather_data = get_outdoor_weather("Lausanne")
+        data.update(weather_data)
+
+        
         q = """INSERT INTO `lab-test-1-415115.weather_IoT_data.weather-records` 
         """
         names = """"""
