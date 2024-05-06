91dd5caecd70b7448e9e28ceb09bc34ed1587e60
Melvin Petracca <38191338+melvin2504@users.noreply.github.com>
2024-05-06 22:03:56 +0200

    Update main.py

--- FlaskApp/main.py
+++ FlaskApp/main.py
@@ -105,7 +105,7 @@ df = query_job.to_dataframe()
 @app.route('/generate_advice_audio', methods=['POST'])
 def generate_advice_audio():
     weather_data = request.get_json(force=True)
-    temperature = weather_data.get(''outdoor_temp')
+    temperature = weather_data.get('outdoor_temp')
     description = weather_data.get('outdoor_weather').lower()
 
     # Generate the base message with current weather description and temperature
