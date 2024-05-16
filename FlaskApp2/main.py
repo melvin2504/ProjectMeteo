f800c9795169f38a43e6386ec0b7526a6eecc42f
Melvin Petracca <38191338+melvin2504@users.noreply.github.com>
2024-05-16 09:39:08 +0200

    Update main.py

--- FlaskApp2/main.py
+++ FlaskApp2/main.py
@@ -4,6 +4,8 @@ from weather import get_weather, get_daily_forecast, weather_icons
 from openai_utils import generate_weather_advice
 from google_cloud_utils import insert_data_to_bigquery, query_latest_weather, query_latest_data
 from config import OPENWEATHER_API_KEY, YOUR_HASH_PASSWD, GCP_PROJECT_ID
+import matplotlib.pyplot as plt
+import pandas as pd
 import os
 
 # Uncomment the line below if you want to run your flask app locally.
