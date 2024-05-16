1a3d03c0a45be23f49f6494f7acc997a7a373082
Melvin Petracca <38191338+melvin2504@users.noreply.github.com>
2024-05-16 09:22:35 +0200

    Update google_cloud_utils.py

--- FlaskApp2/google_cloud_utils.py
+++ FlaskApp2/google_cloud_utils.py
@@ -12,6 +12,22 @@ def insert_data_to_bigquery(client, data):
     query_job = client.query(query)
     query_job.result()  # Wait for the query job to complete
 
+# Function to query the latest data from BigQuery
+def query_latest_data(client, project_id, dataset_id, table_id):
+    query = f"""
+        SELECT *
+        FROM `{project_id}.{dataset_id}.{table_id}`
+        ORDER BY date DESC, time DESC
+        LIMIT 1000
+    """
+    query_job = client.query(query)
+    df = query_job.to_dataframe()
+    now = datetime.now()
+    six_hours_ago = now - timedelta(hours=6)
+    df['datetime'] = pd.to_datetime(df['date'] + ' ' + df['time'])
+    filtered_df = df[df['datetime'] >= six_hours_ago]
+    return filtered_df
+
 def query_latest_weather(client):
     """Fetches the latest weather data from BigQuery."""
     query = """
