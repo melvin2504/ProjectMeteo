8b318fa927cfc664c3f45ac13737e752d312a060
Melvin Petracca <38191338+melvin2504@users.noreply.github.com>
2024-05-26 20:06:39 +0200

    Update config.py

--- FlaskApp/config.py
+++ FlaskApp/config.py
@@ -1,13 +1,13 @@
 # Configuration file for storing constants and configurations
 
 # API keys and secrets
-OPENWEATHER_API_KEY = "6b85e31b08c576ddd0a8f6a60e5afc01"
-YOUR_HASH_PASSWD = "8eac4757d3804403cb4bbd4015df9d2ad252a1e6890605bacb19e5a01a5f2cab"
+OPENWEATHER_API_KEY = "your_openweather_api_key"
+YOUR_HASH_PASSWD = "your_hash_password"
 
 # Google Cloud project ID
-GCP_PROJECT_ID = "lab-test-1-415115"
-DATASET_ID = 'weather_IoT_data'
-TABLE_ID = 'weather-records'
+GCP_PROJECT_ID = "your_gcp_project_id"
+DATASET_ID = 'your_dataset_id'
+TABLE_ID = 'your_table_id'
 
 # OpenAI API key
-OPENAI_API_KEY = "sk-proj-ewEynB9LGERMFCdacv2lT3BlbkFJj8xC95U78WlMP1ZZ9Aiu"
+OPENAI_API_KEY = "your_openai_api_key"
