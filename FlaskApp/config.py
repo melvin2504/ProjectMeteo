29e995b045686225593ba65ac78f87233e03d470
Melvin Petracca <38191338+melvin2504@users.noreply.github.com>
2024-05-26 21:31:54 +0200

    Update config.py

--- FlaskApp/config.py
+++ FlaskApp/config.py
@@ -8,6 +8,7 @@ YOUR_HASH_PASSWD = "your_hash_password"
 GCP_PROJECT_ID = "your_gcp_project_id"
 DATASET_ID = 'your_dataset_id'
 TABLE_ID = 'your_table_id'
+CITY = 'YOUR CITY'
 
 # OpenAI API key
 OPENAI_API_KEY = "your_openai_api_key"
